604cff34b47e85b8a0f8cb37914ff49071a6b8cd
Dayanand Patil <Dayanand.Patil@extentia.com>
2021-12-15 12:16:19 +0530

    useReducer hooks used

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ComponentE from "./ComponentE";
-
+const initialState = {
+    count:0
+}
+const reducer =(state = initialState,action)=>{
+    switch(action.type){
+        case 'INCREMENT' : 
+            return{count:state.count+1}
+        case 'DECREMENT' : 
+            return{count:state.count-1}
+        case 'RESET' : 
+            return initialState
+        default:    
+            return state;
+    }
+}
 
 const ComponentC = () =>{
+    
+    const [state, dispatch] = useReducer(reducer,initialState)
     return(<>
-    <div>ComponentC</div>
+    <div>
+        <div>ComponentC</div>
+        <h5>Increment Counter</h5>
+        <div>Counter {state.count}</div>
+        <button onClick={()=>dispatch({type:'INCREMENT'})}>Increment</button>
+        <button onClick={()=>dispatch({type:'DECREMENT'})}>Decrement</button>
+        <button onClick={()=>dispatch({type:'RESET'})}>Reset</button>
+    </div>
     <ComponentE/>
     </>)
 }
